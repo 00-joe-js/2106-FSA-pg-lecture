c7c0b92ff2a27422ea8535862f699a96f16f54fd
Joe Alves <joseph.michael.alves@gmail.com>
2021-06-24 11:41:37 -0400

    New members page route

--- server.js
+++ server.js
@@ -5,11 +5,41 @@ const client = require("./db-client");
 
 app.get("/facilities-data", async (req, res, next) => {
     try {
-        const facilitiesQueryResult = await client.query(`SELECT * FROM cd.facilities`);
+        const facilitiesQueryResult = await client.query(`SELECT * FROM cd.facilities;`);
         res.send(facilitiesQueryResult.rows);
     } catch (e) {
         next(e);
     }
+});
+
+app.get("/members-page", async (req, res, next) => {
+
+    const membersQueryResult = await client.query(`SELECT firstname, surname FROM cd.members`);
+    
+    const liElements = membersQueryResult.rows.map(eachRow => {
+        return `<li>${eachRow.firstname} ${eachRow.surname}</li>`;
+    }).join("");
+
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+            <head><title>Members Page ${membersQueryResult.rowCount}</title></head>
+            <style>
+                body {
+                    background: gray;
+                }
+                li {
+                    color: blanchedalmond;
+                }
+            </style>
+            <body>
+                <ul>
+                    ${liElements}
+                </ul>
+            </body>
+        </html>
+    `);
+
 });
 
 app.use((err, req, res, next) => {
